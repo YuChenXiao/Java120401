b687b08ef3dc4002d594d60f339331fd9d867e84
XiaoChen <s3310304095@gms.nutc.edu.tw>
2017-12-04 15:08:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/auser/java120401/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.auser.java120401;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
